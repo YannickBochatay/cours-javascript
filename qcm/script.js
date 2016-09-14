5d3792f7ca03d8d02af49635e0273d158724e8e0
Yannick Bochatay <yannick.bochatay@meteo.fr>
2016-09-14 16:56:16 +0200

    qq mises à jour et simplifications

--- qcm/script.js
+++ qcm/script.js
@@ -1,13 +1,13 @@
 (function() {
-	
+
 	"use strict";
-	
+
 	var slice = Array.prototype.slice;
-	
+
 	function $(selector,parent) { return slice.call( (parent ||document).querySelectorAll(selector) ); }
-        
+
         function get(url) {
-		
+
             return new Promise(function(resolve,reject) {
 
                 var req = new XMLHttpRequest();
@@ -24,93 +24,93 @@
                 req.send(null);
             });
 	}
-        
-        
+
+
         var qcm = (function() {
-            
+
             return {
-                
+
                 current : "00",
-                
+
                 nbQuestions : 0,
-                
+
                 nbQCMs : 13,
-                
+
                 change : function(num) {
-                    
+
                     this.current = num;
-                    
+
                     this.enCours = false;
-                    
+
                     document.title = "QCM "+this.current;
-                                        
+
                     return get("chapitres/"+num+".html").then(this._init.bind(this)).catch(alert);
                 },
-                
+
                 _getNomStorage : function() {
-                    
+
                     return "qcm"+this.current;
                 },
-                
+
                 _getFieldSets : function() {
-                  
+
                     return $('form#qcm fieldset');
                 },
-                
+
                 _sauve : function(donnees) {
-                  
+
                     window.localStorage.setItem( this._getNomStorage(), JSON.stringify(donnees) );
                 },
-                
+
                 sauve : function() {
-                  
+
                     var donnees = this._getFieldSets().map(function(fieldset,i) {
-                        
+
                         var inputs = $("input",fieldset),
                             checked = fieldset.querySelector("input:checked");
-                            
-                        return checked ? inputs.indexOf(checked) : -1;   
+
+                        return checked ? inputs.indexOf(checked) : -1;
                     });
-                    
+
                     this._sauve(donnees);
                 },
-                
+
                 _initFieldSets : function(donnees) {
-                    
+
                     this._getFieldSets().forEach(function(fieldset,i) {
-                        
+
                         var inputs = $("input",fieldset);
-                        
+
                         inputs.forEach(function(input) {
-                            
+
                             var label = input.parentNode;
-                            
+
                             input.checked = false;
                             label.classList.remove("ok");
                             label.classList.remove("ko");
                         });
-                        
+
                         if (donnees[i] != undefined && donnees[i] != -1) inputs[ donnees[i] ].checked = true;
                     });
                 },
-                
+
                 recupStorage : function() {
-                    
+
                     var donnees = window.localStorage.getItem( this._getNomStorage() );
-                    
+
                     if (!donnees) return;
-                    
+
                     donnees = JSON.parse(donnees);
-                    
+
                     this._initFieldSets(donnees);
                 },
-                
+
                 reinit : function() {
-                    
+
                     this._initFieldSets([]);
                     this._sauve([]);
                 },
-                               
+
                 _createQCM : function(elmt,i) {
 
                     i++;
@@ -140,7 +140,7 @@
                         var input = document.createElement("input");
                         input.setAttribute("type","radio");
                         input.setAttribute("name","q"+i);
-                        
+
                         if (correct) input.dataset.correct = true;
 
                         label.appendChild(input);
@@ -165,42 +165,35 @@
                             var correct = choix.getAttribute("correct") != undefined;
                             fieldset.appendChild( createInput(text,correct) );
                     });
-                                        
+
                     elmt.parentNode.replaceChild(fieldset,elmt);
                 },
-                                
+
                 _init : function(html) {
-                    
+
                     var div = document.createElement("div");
-                    
+
                     div.innerHTML = html;
-                                        
+
                     var questions = $("qcm",div);
-                    
+
                     questions.forEach(this._createQCM);
-                    
+
                     var parent = document.getElementById("qcm");
-                    
+
                     parent.replaceChild(div,parent.firstElementChild);
-                    
+
                     Prism.highlightAll();
-                    
+
                     this.nbQuestions = questions.length;
-                    
+
                     this.recupStorage();
                 },
-              
-                playSound : function(son) {
-                 
-                    var audio = new Audio();
-                    audio.src = "../wav/"+son+'.wav';
-                    audio.play();
-                },
-                
+
                 calculeBonnesReponses : function() {
-                    
+
                     var nbBonnesReponses = 0;
-				
+
                     $('form#qcm input').forEach(function(input) {
 
                         var parent = input.parentNode;
@@ -215,14 +208,14 @@
                             else parent.classList.add("ko");
                         }
                     });
-                    
+
                     return nbBonnesReponses;
                 },
-                
+
                 genereCommentaire : function(pourcentage) {
-                  
+
                     switch (true) {
-                        
+
                         case (pourcentage < 30) : return "Vous aurez beaucoup de choses à apprendre dans ce cours, accrochez-vous !";
                         case (pourcentage < 50) : return "De nombreux concepts sont à acquérir dans ce cours.";
                         case (pourcentage < 70) : return "Tous les concepts ne sont pas encore clairs, ce cours est fait pour vous !";
@@ -230,44 +223,42 @@
                         case (pourcentage < 100) : return "Vous connaissez la plupart des notions qui seront abordées.";
                         case (pourcentage  == 100) : return "Passez votre chemin, ce cours ne vous apprendra rien.";
                     }
-                    
+
                 },
-                
+
                 enCours : false,
-                
+
                 valide : function() {
-                            
+
                     this.enCours = false;
-                    
-                    this.playSound("ok");
-                    
+
                     var nbBonnesReponses = this.calculeBonnesReponses();
-                    
+
                     var message = "Votre score : "+nbBonnesReponses+" / "+this.nbQuestions;
-                    
+
                     if (this.current == "00") message+= "\n\n"+this.genereCommentaire( 100 * nbBonnesReponses / this.nbQuestions);
-                    
+
                     alert(message);
                 }
             }
-            
+
         }());
-        
+
         var select = document.querySelector('[name=qcm]'),
             suiv = document.getElementById('suiv'),
             prec = document.getElementById('prec');
-            
+
         qcm.numQCMs = select.options.length;
-                
+
         function formateNum(num) { return (num < 10 ? "0" : "") + num; }
-			
+
         function hashchange(e) {
-            
+
             var numQCM = location.hash.slice(1) || "00";
-           
+
             qcm.change(numQCM);
             select.value = numQCM;
-            
+
             numQCM = Number(numQCM);
 
             if (numQCM <= 0) {
@@ -288,27 +279,27 @@
                 suiv.href="#"+formateNum(numQCM+1);
             }
         }
-	
+
         select.addEventListener("change",function() {
             window.location.hash = this.value;
 	});
-        
+
         hashchange();
-        
+
         document.getElementById("qcm").addEventListener("change",function() {
            qcm.enCours = true;
         });
-	
+
         window.addEventListener("hashchange",hashchange);
-        
+
         var form = document.querySelector("form#qcm");
-        				
+
 	form.addEventListener("submit",function(e) {
             e.preventDefault();
             qcm.valide();
 	});
-        
+
         form.addEventListener("change",qcm.sauve.bind(qcm));
-        
-        document.getElementById("reinit").addEventListener("click",qcm.reinit.bind(qcm));        
+
+        document.getElementById("reinit").addEventListener("click",qcm.reinit.bind(qcm));
 }());
