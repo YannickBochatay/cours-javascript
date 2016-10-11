04c938e6d472d058b4b945f7a36a0604f0220b79
ybochatay <yannick.bochatay@free.fr>
2016-10-11 14:49:01 +0200

    faute de frappe

--- exos/exo05/init.js
+++ exos/exo05/init.js
@@ -9,4 +9,4 @@ var obj = { nom : "Toto" };
 modifObjet( obj, { age : 26, nom : "Tata" } );
 
 obj.age; //26
-obj.nom; //Toto (la proopriété existait déjà, elle n'est pas modifiée)
+obj.nom; //Toto (la propriété existait déjà, elle n'est pas modifiée)
