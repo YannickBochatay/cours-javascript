f5c5ac868af43a3c32f0809b3f59f0a8c38efbb5
Yannick Bochatay <yannick.bochatay@meteo.fr>
2022-06-14 11:19:40 +0200

    petites modifs sur les exercices

--- exos/exo04/corrige.js
+++ exos/exo04/corrige.js
@@ -2,7 +2,7 @@ function concat(...args) {
 
   let str ='';
 
-	for (let i=0;i<args.length;i++) str+=args[i];
+  for (let i=0;i<args.length;i++) str+=args[i];
 
   return str;
 }
--- exos/exo05/corrige.js
+++ exos/exo05/corrige.js
@@ -1,6 +1,7 @@
 function modifObjet(obj,props) {
   for (let n in props) {
-    if (n in obj) continue;
-    obj[n] = props[n];
+    if (!(n in obj)) {
+      obj[n] = props[n];
+    }
   }
 }
