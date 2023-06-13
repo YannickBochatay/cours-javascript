d3329c708e0f278a3ce04a33e679c54f6171414e
ybochatay <yannick.bochatay@meteo.fr>
2023-06-13 15:48:30 +0200

    Modification de l'exo5

--- exos/exo05/corrige.js
+++ exos/exo05/corrige.js
@@ -1,7 +1,3 @@
-function modifObjet(obj,props) {
-  for (let n in props) {
-    if (!(n in obj)) {
-      obj[n] = props[n];
-    }
-  }
+function recupProps(props, propsDefaut) {
+  return { ...propsDefault, ...props }
 }
