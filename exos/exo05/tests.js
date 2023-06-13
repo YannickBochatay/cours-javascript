eaad92310742bccd0d2a79007bf03b61e5a047fa
ybochatay <yannick.bochatay@meteo.fr>
2023-06-13 15:48:20 +0200

    Modification de l'exo5

--- exos/exo05/tests.js
+++ exos/exo05/tests.js
@@ -1,10 +1,10 @@
 var _objet = { nom : "Yannick" };
 
-modifObjet(_objet,{ age : 37 });
-assert( _objet.age == 37, "Ajout d'une propriété" );
+var result1 = recupProps(_objet,{ age : 37 });
+assert( result1.age == 37, "Ajout d'une propriété" );
 
-modifObjet(_objet,{ nom : "Bernard" });
-assert( _objet.nom == "Yannick", "une propriété existante n'est pas modifiée" );
+var result2 = recupProps(_objet,{ nom : "Bernard" });
+assert( result2.nom == "Yannick", "une propriété existante n'est pas modifiée" );
 
-modifObjet(_objet,{ sexe : "M", metier : "meteo" });
-assert( _objet.sexe == "M" && _objet.metier == "meteo" && _objet.nom == "Yannick" && _objet.age == 37, "ajout simultané des plusieurs propriétés" );
+var result3 = recupProps(_objet,{ sexe : "M", metier : "meteo" });
+assert( result3.sexe == "M" && result3.metier == "meteo" && result3.nom == "Yannick" && result3.age == 37, "ajout simultané des plusieurs propriétés" );
