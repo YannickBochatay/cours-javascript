a4bb1978bf506453ed0e0fe47b9909503d2dd6ca
BOCHATAY YANNICK <yannick.bochatay@meteo.fr>
2025-06-16 14:56:30 +0200

    Amélioration de la clarté de la correction de l'exo 14

--- exos/exo14/script.js
+++ exos/exo14/script.js
@@ -2,7 +2,8 @@ let lien = document.getElementById("hautDePage");
 
 function checkScroll() {
 
-    lien.classList[ (window.scrollY > 500) ? "remove" : "add" ]("hidden");
+    let methode = (window.scrollY > 500) ? "remove" : "add"
+    lien.classList[methode]("hidden");
 }
 
 window.addEventListener("scroll",checkScroll);
